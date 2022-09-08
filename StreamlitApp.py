246a210e7f04bbc7c225f05b66ed127589a8a357
bildo1935 <60318570+bildo1935@users.noreply.github.com>
2022-09-08 19:16:20 +0800

    Add files via upload

---
+++ StreamlitApp.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+import subprocess
+
+st.title('Welcome to AI4Good.')
+
+st.header('Introduction.')
+st.markdown(f'''
+Recent volatility and turbulence has caused a marked increase in distress of the Singaporean population. 
+It has been found that many who suffer from said mental distress attempt to find relief through (anonymous) participation  
+in online communities e.g. Reddit.
+''')
+
+st.header('How it works.')
+st.markdown(f'''This project mines three subreddits (r/Singapore, r/NationalServiceSG and r/SingaporeRaw) for posts as well as their authors. 
+It preprocesses the text and transforms it via Word2Vec embedddings.
+It is then fed as input into a trained Recurrent Neural Network with Gated Recurrent Units. 
+The output is in the form of two classes (0 or 1 each representing mental stability and a relative lack thereof respectively).
+Authors whose texts are classified as representing psychological instability are flagged up.''')
+
+st.markdown(f'''The dataset used for training is a modified version of the training data used in  
+[On the State of Social Media Data for Mental Health Research](https://arxiv.org/abs/2011.05233).  
+More of such datasets can be found [here](https://github.com/kharrigian/mental-health-datasets).
+''')
+
+roller = st.button('Execute Model.')
+if roller:
+    st.write('This might take a while.')
+    subprocess.check_output(f"{sys.executable}", "project.py")
+
